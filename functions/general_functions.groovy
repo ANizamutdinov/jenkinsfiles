5c9b411d5e1ed5fa07b44b5634d217474bb018f2
Artur Nizamutdinov <artur.nizamutdinov@skuvault.com>
2021-03-26 17:36:22 +0500

    hm...

--- functions/general_functions.groovy
+++ functions/general_functions.groovy
@@ -1,4 +1,5 @@
 def getSas(def storage_account_name, def storage_container_name) {
     answer = "Account:" + storage_account_name + "\nBLOB:" + storage_container_name
+    return answer
 }
-return answer
+return this
