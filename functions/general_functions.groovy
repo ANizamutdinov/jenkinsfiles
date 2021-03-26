b053f7201f198458657ee5f1872c6a5e64a31eeb
Artur Nizamutdinov <artur.nizamutdinov@skuvault.com>
2021-03-26 16:57:21 +0500

    Is this correct?

--- functions/general_functions.groovy
+++ functions/general_functions.groovy
@@ -1,4 +1,4 @@
-def get_sas(storage_account_name, storage_container_name) {
+def get_sas(def storage_account_name, def storage_container_name) {
     node ('master') {
         echo "Account: ${storage_account_name} \n BLOB: ${storage_container_name}"
     }
