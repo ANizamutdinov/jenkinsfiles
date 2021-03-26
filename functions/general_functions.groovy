adad9d795c3a287c4787d96547a63f43d217e398
Artur Nizamutdinov <artur.nizamutdinov@skuvault.com>
2021-03-26 17:22:43 +0500

    Yep, this could be unnecessary

--- functions/general_functions.groovy
+++ functions/general_functions.groovy
@@ -1,5 +1,3 @@
 def getSas(def storage_account_name, def storage_container_name) {
-    node ('master') {
-        println( "Account:" + storage_account_name + "\nBLOB:" + storage_container_name)
-    }
+    println( "Account:" + storage_account_name + "\nBLOB:" + storage_container_name)
 }
