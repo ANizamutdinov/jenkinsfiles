51b92bc9be4bad732ee280197c71b58d2913b560
Artur Nizamutdinov <artur.nizamutdinov@skuvault.com>
2021-03-26 17:18:47 +0500

    And how is this?

--- functions/general_functions.groovy
+++ functions/general_functions.groovy
@@ -1,5 +1,5 @@
-def get_sas(def storage_account_name, def storage_container_name) {
+def getSas(def storage_account_name, def storage_container_name) {
     node ('master') {
-        echo "Account: ${storage_account_name} \n BLOB: ${storage_container_name}"
+        println( "Account:" + storage_account_name + "\nBLOB:" + storage_container_name)
     }
 }
