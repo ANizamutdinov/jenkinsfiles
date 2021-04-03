5742571269b71d0dd66804299a0303473f9cc2fd
Artur Nizamutdinov <artur.nizamutdinov@skuvault.com>
2021-04-03 12:16:29 +0500

    lets take a look deeper

--- functions/general_functions.groovy
+++ functions/general_functions.groovy
@@ -5,8 +5,8 @@ def getSas(def storage_account_name, def storage_container_name) {
                 EXP=$(date -u -d "180 minutes" '+%Y-%m-%dT%H:%M:00Z')
                 NOW=$(date -u '+%Y-%m-%dT%H:%M:00Z')
                 echo "Time now: $NOW \nExp time: $EXP"
-                az login --service-principal -u ''' + AZURE_CLIENT_ID + ''' -p ''' + AZURE_CLIENT_SECRET + ''' -t ''' + AZURE_TENANT_ID ''' > /dev/null 2>&1
-                az account set --subscription ''' + AZURE_SUBSCRIPTION_ID + '''
+                az login --service-principal -u ''' + AZURE_CLIENT_ID + ''' -p ''' + AZURE_CLIENT_SECRET + ''' -t ''' + AZURE_TENANT_ID ''' -o table
+                az account set -s ''' + AZURE_SUBSCRIPTION_ID + ''' -o none
                 TOKEN=$(az storage container generate-sas --account-name ''' + storage_account_name + ''' --n ''' + storage_container_name + ''' --permissions lr --expiry $EXP --https-only)
                 echo ${TOKEN} > ./sas
                 echo "Generated SAS token: ${TOKEN}"
