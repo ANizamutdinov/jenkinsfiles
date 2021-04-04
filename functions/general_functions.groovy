5dbfea1b9d6e9344de948caeb57211efddf63de0
Artur Nizamutdinov <artur.nizamutdinov@skuvault.com>
2021-04-04 11:24:12 +0500

    mispasted

--- functions/general_functions.groovy
+++ functions/general_functions.groovy
@@ -8,12 +8,9 @@ def getContainerSas(def storage_account_name, def storage_container_name) {
         println('Token would be valid from ' + sas_start + ' till '+ sas_end)
         sh '''#!/bin/bash
                 set -e
-                EXP=$(date -u -d "180 minutes" '+%Y-%m-%dT%H:%M:00Z')
-                NOW=$(date -u '+%Y-%m-%dT%H:%M:00Z')
-                echo "Time now: $NOW \nExp time: $EXP"
                 az login --service-principal -u ''' + AZURE_CLIENT_ID + ''' -p ''' + AZURE_CLIENT_SECRET + ''' -t ''' + AZURE_TENANT_ID + ''' -o none
                 az account set -s ''' + AZURE_SUBSCRIPTION_ID + ''' -o none
-                TOKEN=$(az storage container generate-sas --account-name ''' + storage_account_name + ''' --n ''' + storage_container_name + ''' --permissions lr --expiry $EXP --https-only  --as-user --auth-mode login)
+                TOKEN=$(az storage container generate-sas --account-name ''' + storage_account_name + ''' --n ''' + storage_container_name + ''' --expiry ''' + sas_end + ''' --permissions lr --https-only  --as-user --auth-mode login)
                 echo ${TOKEN} > ./sas
                 az logout
                 '''
@@ -29,12 +26,12 @@ def getContainerSas(def storage_account_name, def storage_container_name) {
 }
 def getClassicContainerSas (def storage_container_name) {
     node ('master') {
+        def sas_start = Instant.now()
+        def sas_end = sas_start.plus(Duration.ofHours(3))
+        println('Token would be valid from ' + sas_start + ' till '+ sas_end)
         sh '''#!/bin/bash
                 set -e
-                EXP=$(date -u -d "180 minutes" '+%Y-%m-%dT%H:%M:00Z')
-                NOW=$(date -u '+%Y-%m-%dT%H:%M:00Z')
-                echo "Time now: $NOW \nExp time: $EXP"
-                TOKEN=$(az storage container generate-sas --account-name ''' + AZURE_STORAGE_ACCOUNT_NAME + ''' --account-key ''' + AZURE_STORAGE_ACCOUNT_KEY + ''' --n ''' + storage_container_name + ''' --permissions lr --expiry $EXP --https-only)
+                TOKEN=$(az storage container generate-sas --account-name ''' + AZURE_STORAGE_ACCOUNT_NAME + ''' --account-key ''' + AZURE_STORAGE_ACCOUNT_KEY + ''' --n ''' + storage_container_name + ''' --expiry ''' + sas_end + ''' --permissions lr --https-only)
                 echo ${TOKEN} > ./sas
                 '''
         SAS = readFile("$env.WORKSPACE/sas").replace("\"", "")
