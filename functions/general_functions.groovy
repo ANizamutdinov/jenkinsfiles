2f0136ebad1ca61a1852b2d4b345aee8fbd67756
Artur Nizamutdinov <artur.nizamutdinov@skuvault.com>
2021-04-04 12:35:50 +0500

    ugh.. azure

--- functions/general_functions.groovy
+++ functions/general_functions.groovy
@@ -5,9 +5,9 @@ def getContainerSasByLogin(def storage_account_name, def storage_container_name)
                 EXP=$(date -u -d "180 minutes" '+%Y-%m-%dT%H:%M:00Z')
                 NOW=$(date -u '+%Y-%m-%dT%H:%M:00Z')
                 echo "Time now: $NOW \nExp time: $EXP"
-                az login --service-principal -u ''' + AZURE_CLIENT_ID + ''' -p ''' + AZURE_CLIENT_SECRET + ''' -t ''' + AZURE_TENANT_ID + ''' -o table
+                az login --service-principal -u ''' + AZURE_CLIENT_ID + ''' -p ''' + AZURE_CLIENT_SECRET + ''' -t ''' + AZURE_TENANT_ID + ''' -o none
                 az account set -s ''' + AZURE_SUBSCRIPTION_ID + ''' -o none
-                TOKEN=$(az storage container generate-sas --account-name ''' + storage_account_name + ''' --n ''' + storage_container_name + ''' --permissions lr --expiry $EXP --as-user --auth-mode login --https-only)
+                TOKEN=$(az storage container generate-sas --account-name ''' + storage_account_name + ''' --n ''' + storage_container_name + ''' --permissions lr --expiry $EXP --https-only)
                 echo ${TOKEN} > ./sas
                 az logout
                 '''
