3d50820f6102715f326b426a5028e1a7abdd7cca
Artur Nizamutdinov <artur.nizamutdinov@skuvault.com>
2021-04-04 14:59:54 +0500

    and... sed

--- functions/general_functions.groovy
+++ functions/general_functions.groovy
@@ -7,7 +7,7 @@ def getContainerSasByLogin(def storage_account_name, def storage_container_name)
                 echo "Time now: $NOW \nExp time: $EXP"
                 az login --service-principal -u ''' + AZURE_CLIENT_ID + ''' -p ''' + AZURE_CLIENT_SECRET + ''' -t ''' + AZURE_TENANT_ID + ''' -o none
                 az account set -s ''' + AZURE_SUBSCRIPTION_ID + ''' -o none
-                TOKEN=$(az storage container generate-sas --account-name ''' + storage_account_name + ''' --n ''' + storage_container_name + ''' --permissions lr --expiry $EXP --https-only)
+                TOKEN=$(az storage container generate-sas --account-name ''' + storage_account_name + ''' --n ''' + storage_container_name + ''' --permissions lr --expiry $EXP --https-only | sed 's/%3A/:/g')
                 echo ${TOKEN} > ./sas
                 az logout
                 '''
@@ -29,7 +29,7 @@ def getContainerSasByAccountKey(def storage_container_name) {
                 EXP=$(date -u -d "180 minutes" '+%Y-%m-%dT%H:%M:00Z')
                 NOW=$(date -u '+%Y-%m-%dT%H:%M:00Z')
                 echo "Time now: $NOW \nExp time: $EXP"
-                TOKEN=$(az storage container generate-sas --account-name ''' + AZURE_STORAGE_ACCOUNT_NAME + ''' --account-key ''' + AZURE_STORAGE_ACCOUNT_KEY + ''' --n ''' + storage_container_name + ''' --permissions lr --expiry $EXP --https-only)
+                TOKEN=$(az storage container generate-sas --account-name ''' + AZURE_STORAGE_ACCOUNT_NAME + ''' --account-key ''' + AZURE_STORAGE_ACCOUNT_KEY + ''' --n ''' + storage_container_name + ''' --permissions lr --expiry $EXP --https-only | sed 's/%3A/:/g')
                 echo ${TOKEN} > ./sas
                 '''
         SAS = readFile("$env.WORKSPACE/sas").replace("\"", "")
