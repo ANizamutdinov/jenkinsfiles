0456658b20ba203bc9f81ce1320b50aaffba38b0
Artur Nizamutdinov <artur.nizamutdinov@skuvault.com>
2021-04-04 10:31:29 +0500

    looks I got it

--- functions/general_functions.groovy
+++ functions/general_functions.groovy
@@ -1,4 +1,4 @@
-def getSas(def storage_account_name, def storage_container_name) {
+def getContainerSas(def storage_account_name, def storage_container_name) {
     node ('master') {
         sh '''#!/bin/bash
                 set -x
@@ -16,4 +16,21 @@ def getSas(def storage_account_name, def storage_container_name) {
         return SAS
     }
 }
+def getClassicContainerSas (def storage_account_name, def storage_container_name) {
+    node ('master') {
+        sh '''#!/bin/bash
+                set -x
+                EXP=$(date -u -d "180 minutes" '+%Y-%m-%dT%H:%M:00Z')
+                NOW=$(date -u '+%Y-%m-%dT%H:%M:00Z')
+                echo "Time now: $NOW \nExp time: $EXP"
+                TOKEN=$(az storage container generate-sas --account-name ''' + storage_account_name + ''' --n ''' + storage_container_name + ''' --permissions lr --expiry $EXP --https-only)
+                echo ${TOKEN} > ./sas
+                echo "Generated SAS token: ${TOKEN}"
+                az logout
+                '''
+        SAS = readFile("$env.WORKSPACE/sas").replace("\"", "")
+        return SAS
+    }
+}
+
 return this
